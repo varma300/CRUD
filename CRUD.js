8eba048b7b58f787f263099d7726b6dcb2dbd86a
Rahul Varma <rahulvarma300@gmail.com>
2023-12-12 23:49:28 +0530

    Edit Delete

--- CRUD.js
+++ CRUD.js
@@ -5,9 +5,9 @@ function onFormSubmit(e) {
     if (selectedraw == null) {
         inserNewRecord(formData)
     } else {
-
+        udateRecord(formData);
     }
-
+    resetForm();
 }
 
 //retrive the data
@@ -36,5 +36,43 @@ function inserNewRecord(data) {
     var cell4 = newRaw.insertCell(3);
     cell4.innerHTML = data.price;
     var cell5 = newRaw.insertCell(4);
-    cell5.innerHTML = `<button>Edit</button> <button>Delete</button>`
+    cell5.innerHTML = `<button onclick='onEdit(this)'>Edit</button> <button onclick='onDelete(this)'>Delete</button>`
+}
+
+
+//Edit data
+function onEdit(td){
+    selectedraw = td.parentElement.parentElement;
+    document.getElementById('productCode').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('productName').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('productQty').value = selectedRow.cells[2].innerHTML;
+    document.getElementById('price').value = selectedRow.cells[3].innerHTML;
+
+}
+
+function udateRecord(formData){
+    selectedRow.cells[0].innerHTML = formData.productCode;
+    selectedRow.cells[1].innerHTML = formData.productName;
+    selectedRow.cells[2].innerHTML = formData.productQty;
+    selectedRow.cells[3].innerHTML = formData.price;
+}
+
+
+//delet data
+function onDelet(td){
+    if(confirm('Do you want to dele the record ??')){
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex); 
+
+    }
+    resetForm();
+}
+
+// Reset the data
+
+function resetForm(){
+    document.getElementById('productCode').value = '';
+    document.getElementById('productName').value = '';
+    document.getElementById('productQty').value = '';
+    document.getElementById('price').value ='';
 }
